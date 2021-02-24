5639ea4a005ae78f8cbd5ffe567cc379a7b663ee
SanketRathod17 <sanket.rathod17@vit.edu>
2021-02-24 15:24:58 +0530

    Error for Test Case 3(Comma Separated) solved

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,20 @@
-public class Calculator {
-public int Add(String a)
+public class Calculator
 {
-    if(a.isEmpty())
-        return 0;
-    else
-        return Integer.parseInt(a);
-}
+    public int Add(String a)
+    {
+        String carr[] = a.split(",");
+
+        if (a.isEmpty())
+        {
+            return 0;
+        }
+        else if(carr.length==1)
+        {
+            return Integer.parseInt(a);
+        }
+        else {
+            return (Integer.parseInt(carr[0])+Integer.parseInt(carr[1]));
+             }
+
+    }
 }
